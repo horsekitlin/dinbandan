838eaa177081d62aa18c53ea9598f3b226f42663
horsekitlin <horsekit1982@gmail.com>
2017-12-30 16:33:53 +0800

    增加追蹤 config.js

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCJshSB2O3ZG84vRqT9hOMJPDtQXQqOY7U",
+  authDomain: "dinbandan-46e8c.firebaseapp.com",
+  databaseURL: "https://dinbandan-46e8c.firebaseio.com",
+  storageBucket: "dinbandan-46e8c.appspot.com",
+  messagingSenderId: "544525397793",
+  projectId: "dinbandan-46e8c"
+};
+
+export default {
+  firebaseConfig
+};
